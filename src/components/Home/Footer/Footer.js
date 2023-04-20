1da5f20049d7ca736f2d3a0535e278160c63b127
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-04-20 08:31:13 +0000

    solve lab 11

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,13 @@
+function Footer(){
+return(
+    <>
+    <p> Phone : *****</p>
+    <p> Email : ****@gamail.com</p>
+    <p> &copy 2001</p>
+    <p> Auther Name : Tasneem Alabed</p>
+    </>
+)
+
+}
+
+export default Footer;
